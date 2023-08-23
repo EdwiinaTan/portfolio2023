2e45ad548fe11ed2259ac1556c40667e426fd258
EdwiinaTan <edwina.tan77@gmail.com>
2023-08-23 21:44:16 +0200

    Add Mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@mui/material"
+import { useState } from "react"
+import "./App.css"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <Button variant="text">Text</Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
