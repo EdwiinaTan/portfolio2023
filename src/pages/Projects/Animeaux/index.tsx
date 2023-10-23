bc54aa6e9b8640d807d13f55dfa1d11199484f93
EdwiinaTan <edwina.tan77@gmail.com>
2023-10-23 23:43:17 +0200

    Add hifi img

--- src/pages/Projects/Animeaux/index.tsx
+++ src/pages/Projects/Animeaux/index.tsx
@@ -10,10 +10,30 @@ import { Presentation } from "./Presentation"
 import { Tech } from "./Tech"
 import banner from "/assets/animeaux/banner.png"
 import animalEdit1 from "/assets/animeaux/hifi/animalEdit1.svg"
+import animalEdit2 from "/assets/animeaux/hifi/animalEdit2.svg"
+import animalEdit3 from "/assets/animeaux/hifi/animalEdit3.svg"
 import animalProfil from "/assets/animeaux/hifi/animalProfil.svg"
 import animaux from "/assets/animeaux/hifi/animaux.svg"
+import center1 from "/assets/animeaux/hifi/center1.svg"
+import center2 from "/assets/animeaux/hifi/center2.svg"
+import center3 from "/assets/animeaux/hifi/center3.svg"
+import center4 from "/assets/animeaux/hifi/center4.svg"
+import center5 from "/assets/animeaux/hifi/center5.svg"
+import center6 from "/assets/animeaux/hifi/center6.svg"
+import center7 from "/assets/animeaux/hifi/center7.svg"
 import connexion from "/assets/animeaux/hifi/connexion.svg"
 import createAccount from "/assets/animeaux/hifi/createAccount.svg"
+import famille1 from "/assets/animeaux/hifi/famille1.svg"
+import famille2 from "/assets/animeaux/hifi/famille2.svg"
+import famille3 from "/assets/animeaux/hifi/famille3.svg"
+import famille4 from "/assets/animeaux/hifi/famille4.svg"
+import famille5 from "/assets/animeaux/hifi/famille5.svg"
+import info1 from "/assets/animeaux/hifi/info1.svg"
+import info2 from "/assets/animeaux/hifi/info2.svg"
+import info3 from "/assets/animeaux/hifi/info3.svg"
+import profil1 from "/assets/animeaux/hifi/profil1.svg"
+import profil2 from "/assets/animeaux/hifi/profil2.svg"
+import profil3 from "/assets/animeaux/hifi/profil3.svg"
 import lofi1 from "/assets/animeaux/lofi1.svg"
 import lofi2 from "/assets/animeaux/lofi2.svg"
 import persona1 from "/assets/animeaux/persona1.svg"
@@ -132,7 +152,7 @@ export const Animeaux = () => {
         <Box sx={{ p: 2 }}></Box>
         <Body4>Page animal</Body4>
         <Box sx={{ p: 0.5 }}></Box>
-        <div style={{ display: "flex", gap: 24 }}>
+        <div style={{ display: "flex", gap: 24, flexWrap: "wrap" }}>
           <img
             src={animaux}
             alt="Animeaux animal association"
@@ -147,6 +167,146 @@ export const Animeaux = () => {
           />
           <img
             src={animalEdit1}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={animalEdit2}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={animalEdit3}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+        </div>
+        <Box sx={{ p: 1 }}></Box>
+        <Body4>Page de famille d'accueil</Body4>
+        <Box sx={{ p: 0.5 }}></Box>
+        <div style={{ display: "flex", gap: 24, flexWrap: "wrap" }}>
+          <img
+            src={famille1}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={famille2}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={famille3}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={famille4}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={famille5}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+        </div>
+        <Box sx={{ p: 1 }}></Box>
+        <Body4>Page d'ajout</Body4>
+        <Box sx={{ p: 0.5 }}></Box>
+        <div style={{ display: "flex", gap: 24, flexWrap: "wrap" }}>
+          <img
+            src={center1}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={center2}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={center3}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={center4}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={center5}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={center6}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={center7}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+        </div>
+        <Box sx={{ p: 1 }}></Box>
+        <Body4>Page information</Body4>
+        <Box sx={{ p: 0.5 }}></Box>
+        <div style={{ display: "flex", gap: 24, flexWrap: "wrap" }}>
+          <img
+            src={info1}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={info2}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={info3}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+        </div>
+        <Box sx={{ p: 1 }}></Box>
+        <Body4>Page profil</Body4>
+        <Box sx={{ p: 0.5 }}></Box>
+        <div style={{ display: "flex", gap: 24, flexWrap: "wrap" }}>
+          <img
+            src={profil1}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={profil2}
+            alt="Animeaux animal association"
+            // width="50%"
+            height={500}
+          />
+          <img
+            src={profil3}
             alt="Animeaux animal association"
             // width="50%"
             height={500}
